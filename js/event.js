acee6c4519e1bc7ae83fd09f2ab2a215826c90b4
Sunan13208 <sunanrabbi1918@gmail.com>
2023-01-13 20:08:47 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,18 @@
+function makered(){
+
+    document.body.style.backgroundColor = 'red'
+}
+
+function makegreen(){
+
+    document.body.style.backgroundColor = 'green'
+}
+
+function makeblue(){
+
+    document.body.style.backgroundColor = 'blue'
+}
+function makeyellow(){
+
+    document.body.style.backgroundColor = 'yellow'
+}
